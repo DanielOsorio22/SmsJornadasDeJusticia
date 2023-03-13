889905b92d3ec9fead61dd6dafc4fd0ee4708368
Tiancris <cristian.osorio@doublevpartners.com>
2023-03-12 22:10:40 -0500

    Last changes updaed, App working fine

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
+// imports
 const express = require('express');
-const path = require('path');
+const http = require('http');
 const bodyParser = require('body-parser');
+
 // Env variables
 require('dotenv').config();
 
+// Twilio
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 // App from express
 const app = express();
-
-// Public folder
-const publicPath = path.resolve(__dirname, './public');
-
-// Twilio Credentials
-const accountSid = 'AC3ddb199da8721711e39cbd74c879e5fa';
-const authToken = '40a74c14da439c86abf530f9db719d75';
-
-// twilio client
-const client = require('twilio')(accountSid, authToken);
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route POST /sms
 app.post('/sms', (req, res) => {
-  const message = req.body.Body;
-  const phone_number = req.body.From;
 
-  // Send a reply
+  //! Send a sms
+  /*  const accountSid = 'AC3ddb199da8721711e39cbd74c879e5fa';
+  const authToken = 'da68a870a10b846cc3425997f94c714a';
+  const client = require('twilio')(accountSid, authToken);
+  
   client.messages
-    .create({
-      body: `Hello, ${phone_number}! Thanks for your message.`,
-      from: 'your_twilio_number',
-      to: phone_number
-    })
-    .then(message => console.log(message.sid));
-
-  res.end();
-});
-
-/* client.messages
   .create({
     body: 'Hello from Twilio!',
     from: '+15673671902',
@@ -46,13 +31,23 @@ app.post('/sms', (req, res) => {
   })
   .then(message => console.log(message.sid)); */
 
+  //! When reciving a sms send the repl
+  /*  const twiml = new MessagingResponse();
+   twiml.message('Thanks for your message!');
+ 
+   res.writeHead(200, { 'Content-Type': 'text/xml' });
+   res.end(twiml.toString()); */
 
-app.use(express.static(publicPath));
+});
 
 // App running in port 3000
 app.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
   console.log('Servidor corriendo en puerto', process.env.PORT);
 });
+
+
+
+
 
 
